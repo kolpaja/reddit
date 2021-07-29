f1a9a8190385ee770580c24c4d9321559b59c3a5
Sokol Paja <kolpaja28@gmail.com>
2021-07-29 15:54:20 +0200

    i

--- src/components/posts-preview/posts-preview.jsx
+++ src/components/posts-preview/posts-preview.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./posts-preview.scss";
 
 import { PostedTime } from "../../utilities/functions";
